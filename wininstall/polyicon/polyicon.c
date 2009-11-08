1e813a2e35a56be9669b1b747738a51916615f5b
David Matthews <dm@prolingua.co.uk>
2009-11-08 12:02:40 +0000

    Added code to generate a Windows Installer (.msi) script under DevStudio.  Requires WiX to be installed.
    git-svn-id: svn://svn.code.sf.net/p/polyml/code/trunk/polyml@928 ae7d391e-3f74-4a2b-a0db-a8776840fd2a

---
+++ wininstall/polyicon/polyicon.c
@@ -0,0 +1,8 @@
+#include <windows.h>
+
+// This is a dummy executable.  It's only needed because the installer
+// needs an executable to hold the icon.
+int __stdcall WinMain(HINSTANCE hI, HINSTANCE hP, LPSTR s, int n)
+{
+    return 0;
+}
