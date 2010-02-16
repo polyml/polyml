7f74bbfb098d7576882ae58165acbe0a5ca431bb
David Matthews <dm@prolingua.co.uk>
2010-02-16 17:36:31 +0000

    Added string.h include file.
    git-svn-id: svn://svn.code.sf.net/p/polyml/code/trunk/polyml@1046 ae7d391e-3f74-4a2b-a0db-a8776840fd2a

--- libpolyml/arb.cpp
+++ libpolyml/arb.cpp
@@ -60,6 +60,10 @@ Thanks are due to D. Knuth for the long division algorithm.
 #include <stdlib.h>
 #endif
 
+#ifdef HAVE_STRING_H
+#include <string.h>
+#endif
+
 #ifdef HAVE_ASSERT_H
 #include <assert.h>
 #define ASSERT(x)   assert(x)
