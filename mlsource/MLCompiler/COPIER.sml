polyml/polyml

commit 2552bc7e4f62090c07c90ee222dd28816ccbda4b

Move arity from type constructor to type identifier. This also required some changes to the way datatypes are printed.

mlsource/MLCompiler/COPIER.sml
@@ -113,7 +113,7 @@ struct
                         val copiedId =
                             TypeId{idKind=TypeFn(args, copiedEquiv), access=access, description=description}
                     in
-                        makeTypeConstructor(makeName(tcName tcon), args, copiedId, tcLocations tcon) :: rest
+                        makeTypeConstructor(makeName(tcName tcon), copiedId, tcLocations tcon) :: rest
                     end
 
                 |   id =>
@@ -124,8 +124,7 @@ struct
                             NONE => rest (* Skip (or add to cache?) *)
                         |   SOME newId =>
                             makeTypeConstructor
-                                (makeName(tcName tcon),
-                                    tcTypeVars tcon, newId, tcLocations tcon) :: rest
+                                (makeName(tcName tcon), newId, tcLocations tcon) :: rest
                     )
             end
             else rest

mlsource/MLCompiler/DATATYPE_REP.ML
@@ -419,7 +419,7 @@ struct
 (*****************************************************************************)
 
     (* Build a datatype within the basis. *)
-    fun buildBasisDatatype(tcName, tIdPath, tyVars, isEqType: bool,
+    fun buildBasisDatatype(tcName, tIdPath, arity: int, isEqType: bool,
             mkValConstrs: typeConstrs -> (values * codetree) list * codetree * codetree) =
     let
         (* Create a temporary datatype.  The "name" we put in here is usually the
@@ -427,9 +427,9 @@ struct
            which have the PolyML prefix. *)
         val description = basisDescription tIdPath
         val id =
-            makeBoundId(Local{addr = ref ~1, level = ref baseLevel}, 0 (* IdNumber*),
+            makeBoundId(arity, Local{addr = ref ~1, level = ref baseLevel}, 0 (* IdNumber*),
                 isEqType, true, description)
-        val dtype = makeTypeConstructor (tcName, tyVars, id, [DeclaredAt inBasis]);
+        val dtype = makeTypeConstructor (tcName, id, [DeclaredAt inBasis]);
         (* Build the constructors. *)
         val (valConstrsAndDecs, boxedCode, sizeCode) = mkValConstrs dtype
         (* The constructors have to be ordered as in genValueConstrs in PARSE_TREE. *)
@@ -464,7 +464,7 @@ struct
                 mkEnv(
                     declConstrs @ TypeVarMap.getCachedTypeValues defMap @ dtCode, mkTuple(codeId(id, baseLevel) :: loadConstrs)),
                 [], !addrs)()
-        val newId = makeFreeId(Global(mkInd(0, globalCode)), isEqType, description)
+        val newId = makeFreeId(arity, Global(mkInd(0, globalCode)), isEqType, description)
         
         (* Get the value constructors out as globals. *)
         fun mkGlobal((Value{name, typeOf, class, locations,  ...}, _), (decs, offset)) =
@@ -482,8 +482,7 @@ struct
     local
         fun makeConsAndNil listType =
         let
-            val listTypeVars  = tcTypeVars listType;
-            val alpha         = TypeVar(hd listTypeVars);
+            val alpha = mkTypeVar(generalisable, false, false, false)
             val alphaList     = mkTypeConstruction ("list", listType, [alpha], [DeclaredAt inBasis]);
             val consType      = mkFunctionType (mkProductType [alpha, alphaList], alphaList);
             val nilConstructor  =
@@ -499,10 +498,7 @@ struct
         end
     in
         val listConstr =
-            buildBasisDatatype("list", "list",
-                [makeTv {value=EmptyType, level=generalisable, nonunifiable=false,
-                         equality=false, printable=false}],
-                  true, makeConsAndNil)
+            buildBasisDatatype("list", "list", 1, true, makeConsAndNil)
         val (nilConstructor, consConstructor) =
             case listConstr of
                 TypeConstrSet(_, [consC as Value{name="::", ...}, nilC as Value{name="nil", ...}]) =>
@@ -513,8 +509,7 @@ struct
     local
         fun makeNoneAndSome optionType =
         let
-            val optionTypeVars  = tcTypeVars optionType;
-            val alpha         = TypeVar(hd optionTypeVars);
+            val alpha = mkTypeVar(generalisable, false, false, false)
             val alphaOption   = mkTypeConstruction ("option", optionType, [alpha], [DeclaredAt inBasis]);
             val someType      = mkFunctionType (alpha, alphaOption);
             val noneConstructor  =
@@ -530,9 +525,7 @@ struct
         end
     in
         val optionConstr=
-            buildBasisDatatype("option", "option",
-                [makeTv {value=EmptyType, level=generalisable, nonunifiable=false,
-                         equality=false, printable=false}], true, makeNoneAndSome)
+            buildBasisDatatype("option", "option", 1, true, makeNoneAndSome)
         val (noneConstructor, someConstructor) =
             case optionConstr of
                 TypeConstrSet(_, [noneC as Value{name="NONE", ...}, someC as Value{name="SOME", ...}]) => (noneC, someC)
@@ -560,8 +553,7 @@ struct
                 ]
         in
             val locationCons =
-                makeTypeConstructor(
-                    "location", [],
+                makeTypeConstructor("location",
                     makeTypeFunction(basisDescription "location", ([], mkLabelled(sortLabels fields, true))),
                     [DeclaredAt inBasis])
             val locationConstr = TypeConstrSet(locationCons, [])
@@ -588,7 +580,7 @@ struct
             end
         in
             val contextConstr =
-                buildBasisDatatype("context", "PolyML.context", [], false, makeConstructors)
+                buildBasisDatatype("context", "PolyML.context", 0, false, makeConstructors)
         end
 
         local
@@ -623,7 +615,7 @@ struct
             end
         in
             val prettyConstr =
-                buildBasisDatatype("pretty", "PolyML.pretty", [], false, makeConstructors)
+                buildBasisDatatype("pretty", "PolyML.pretty", 0, false, makeConstructors)
         end
     end
 

commit 98df1d502d03798a568d49363e06bfd95d08c218

Fix printing of datatypes with equality type variables. This was broken in 2552bc7e4f62090c07c90ee222dd28816ccbda4b because the newly constructed type variables did not have any equality setting.  This commit partially reverts that commit.

mlsource/MLCompiler/COPIER.sml
@@ -113,7 +113,7 @@ struct
                         val copiedId =
                             TypeId{idKind=TypeFn(args, copiedEquiv), access=access, description=description}
                     in
-                        makeTypeConstructor(makeName(tcName tcon), copiedId, tcLocations tcon) :: rest
+                        makeTypeConstructor(makeName(tcName tcon), args, copiedId, tcLocations tcon) :: rest
                     end
 
                 |   id =>
@@ -124,7 +124,7 @@ struct
                             NONE => rest (* Skip (or add to cache?) *)
                         |   SOME newId =>
                             makeTypeConstructor
-                                (makeName(tcName tcon), newId, tcLocations tcon) :: rest
+                                (makeName(tcName tcon), tcTypeVars tcon, newId, tcLocations tcon) :: rest
                     )
             end
             else rest

mlsource/MLCompiler/DATATYPE_REP.ML
@@ -415,17 +415,18 @@ struct
 (*****************************************************************************)
 
     (* Build a datatype within the basis. *)
-    fun buildBasisDatatype(tcName, tIdPath, arity: int, isEqType: bool,
+    fun buildBasisDatatype(tcName, tIdPath, tyVars, isEqType: bool,
             mkValConstrs: typeConstrs -> (values * codetree) list * codetree * codetree) =
     let
         (* Create a temporary datatype.  The "name" we put in here is usually the
            same as the type constructor name except for datatypes in the PolyML structure
            which have the PolyML prefix. *)
+        val arity = List.length tyVars
         val description = basisDescription tIdPath
         val id =
             makeBoundId(arity, Local{addr = ref ~1, level = ref baseLevel}, 0 (* IdNumber*),
                 isEqType, true, description)
-        val dtype = makeTypeConstructor (tcName, id, [DeclaredAt inBasis]);
+        val dtype = makeTypeConstructor (tcName, tyVars, id, [DeclaredAt inBasis]);
         (* Build the constructors. *)
         val (valConstrsAndDecs, boxedCode, sizeCode) = mkValConstrs dtype
         (* The constructors have to be ordered as in genValueConstrs in PARSE_TREE. *)
@@ -478,7 +479,8 @@ struct
     local
         fun makeConsAndNil listType =
         let
-            val alpha = mkTypeVar(generalisable, false, false, false)
+            val listTypeVars  = tcTypeVars listType;
+            val alpha         = TypeVar(hd listTypeVars);
             val alphaList     = mkTypeConstruction ("list", listType, [alpha], [DeclaredAt inBasis]);
             val consType      = mkFunctionType (mkProductType [alpha, alphaList], alphaList);
             val nilConstructor  =
@@ -494,7 +496,10 @@ struct
         end
     in
         val listConstr =
-            buildBasisDatatype("list", "list", 1, true, makeConsAndNil)
+            buildBasisDatatype("list", "list",
+                [makeTv {value=EmptyType, level=generalisable, nonunifiable=false,
+                         equality=false, printable=false}],
+                    true, makeConsAndNil)
         val (nilConstructor, consConstructor) =
             case listConstr of
                 TypeConstrSet(_, [consC as Value{name="::", ...}, nilC as Value{name="nil", ...}]) =>
@@ -505,7 +510,8 @@ struct
     local
         fun makeNoneAndSome optionType =
         let
-            val alpha = mkTypeVar(generalisable, false, false, false)
+            val optionTypeVars  = tcTypeVars optionType;
+            val alpha         = TypeVar(hd optionTypeVars);
             val alphaOption   = mkTypeConstruction ("option", optionType, [alpha], [DeclaredAt inBasis]);
             val someType      = mkFunctionType (alpha, alphaOption);
             val noneConstructor  =
@@ -521,7 +527,9 @@ struct
         end
     in
         val optionConstr=
-            buildBasisDatatype("option", "option", 1, true, makeNoneAndSome)
+            buildBasisDatatype("option", "option",
+                [makeTv {value=EmptyType, level=generalisable, nonunifiable=false,
+                         equality=false, printable=false}], true, makeNoneAndSome)
         val (noneConstructor, someConstructor) =
             case optionConstr of
                 TypeConstrSet(_, [noneC as Value{name="NONE", ...}, someC as Value{name="SOME", ...}]) => (noneC, someC)
@@ -549,7 +557,7 @@ struct
                 ]
         in
             val locationCons =
-                makeTypeConstructor("location",
+                makeTypeConstructor("location", [],
                     makeTypeFunction(basisDescription "PolyML.location", ([], mkLabelled(sortLabels fields, true))),
                     [DeclaredAt inBasis])
             val locationConstr = TypeConstrSet(locationCons, [])
@@ -576,7 +584,7 @@ struct
             end
         in
             val contextConstr =
-                buildBasisDatatype("context", "PolyML.context", 0, false, makeConstructors)
+                buildBasisDatatype("context", "PolyML.context", [], false, makeConstructors)
         end
 
         local
@@ -611,7 +619,7 @@ struct
             end
         in
             val prettyConstr =
-                buildBasisDatatype("pretty", "PolyML.pretty", 0, false, makeConstructors)
+                buildBasisDatatype("pretty", "PolyML.pretty", [], false, makeConstructors)
         end
     end
 

mlsource/MLCompiler/DEBUGGER_.sml
@@ -180,7 +180,7 @@ struct
         let
             val typeID = searchType state (tcIdentifier tcons)
             val newTypeCons =
-                makeTypeConstructor(tcName tcons, typeID, tcLocations tcons)
+                makeTypeConstructor(tcName tcons, tcTypeVars tcons, typeID, tcLocations tcons)
 
             val newValConstrs = (*map copyAConstructor tcConstructors*) []
         in

mlsource/MLCompiler/INITIALISE_.ML
@@ -111,6 +111,9 @@ struct
             fun makeEqTV  () = mkTypeVar (generalisable, true,  false, false)
             fun makeTV    () = mkTypeVar (generalisable, false, false, false)
             fun makePrintTV() = mkTypeVar (generalisable, false,  false, true)
+            fun makeTypeVariable() =
+                makeTv {value=emptyType, level=generalisable, equality=false,
+                        nonunifiable=false, printable=false}
             
             (* Make some functions *)
             infixr 5 ->>
@@ -163,14 +166,14 @@ struct
         
         fun makeTypeAbbreviation(name, fullName, typeVars, typeResult, locations) =
             makeTypeConstructor(
-                name, makeTypeFunction(basisDescription fullName, (typeVars, typeResult)),
+                name, typeVars, makeTypeFunction(basisDescription fullName, (typeVars, typeResult)),
                 locations)
 
         (* Make an opaque type and add it to an environment. *)
         fun makeAndDeclareOpaqueType(typeName, fullName, env) =
         let
             val typeconstr =
-                makeTypeConstructor(typeName,
+                makeTypeConstructor(typeName, [],
                     makeFreeId(0, defaultEqAndPrintCode(), false, basisDescription fullName),
                     declInBasis);
         in
@@ -593,7 +596,7 @@ struct
                    Global (genCode(code, [], 0) ())
                 end
             val largeWordType =
-                makeTypeConstructor("word",
+                makeTypeConstructor("word", [],
                     makeFreeId(0, idCode, true, basisDescription "word"), declInBasis)
 
             val LargeWord = mkTypeConstruction ("LargeWord.word", largeWordType, [], declInBasis)
@@ -797,7 +800,7 @@ struct
             (* Thread.thread type.  This is an equality type with pointer equality. *)
             val threadConstr= 
                 makeTypeConstructor (
-                    "thread", makeFreeId(0, Global (genCode(code, [], 0) ()), true, basisDescription "thread"),
+                    "thread", [], makeFreeId(0, Global (genCode(code, [], 0) ()), true, basisDescription "thread"),
                     [DeclaredAt inBasis])
             val threadType = mkTypeConstruction ("thread", threadConstr, [], []);
             val selfFunction = mkGvar ("self", Unit ->> threadType, getCurrentThreadIdFn, declInBasis)
@@ -984,7 +987,7 @@ struct
                 end
         in
             val vectorType =
-                makeTypeConstructor("vector",
+                makeTypeConstructor("vector", [makeTypeVariable()],
                     makeFreeId(1, idCode, true, basisDescription "vector"), declInBasis)
             val () = enterGlobalType  ("vector", TypeConstrSet(vectorType, []))
         end
@@ -1033,7 +1036,7 @@ struct
                 end
         in
             val byteVectorType =
-                makeTypeConstructor("byteVector",
+                makeTypeConstructor("byteVector", [],
                     makeFreeId(0, idCode, true, basisDescription "byteVector"), declInBasis)
             val () = #enterType bootstrapEnv ("byteVector", TypeConstrSet(byteVectorType, []))
         end
@@ -1185,7 +1188,7 @@ struct
             (* Entries in the static list.  This type is only used within the implementation of
                DebuggerInterface in the basis library and does not appear in the final signature. *)
             val environEntryConstr =
-                makeTypeConstructor("environEntry",
+                makeTypeConstructor("environEntry", [],
                     makeFreeId(0, defaultEqAndPrintCode(), false,
                         basisDescription "PolyML.DebuggerInterface.environEntry"), declInBasis)
             val environEntryType =
@@ -1355,14 +1358,14 @@ struct
             in
                 (* type 'a tag *)
                 val tagConstr =
-                    makeTypeConstructor("tag",
+                    makeTypeConstructor("tag", [makeTypeVariable()],
                         makeFreeId(1, idCode, false, basisDescription "tag"), declInBasis);
                 val () = #enterType uniStructEnv ("tag", TypeConstrSet(tagConstr, []))
             end
 
             (* type universal *)
             val univConstr =
-                makeTypeConstructor("universal",
+                makeTypeConstructor("universal", [],
                         makeFreeId(0, defaultEqAndPrintCode(), false, basisDescription "universal"), declInBasis);
             val () = #enterType uniStructEnv ("universal",  TypeConstrSet(univConstr, []));
 
@@ -1396,7 +1399,7 @@ struct
             open TYPETREE
             (* Parsetree properties datatype. *)
             val propConstr =
-                makeTypeConstructor("ptProperties",
+                makeTypeConstructor("ptProperties", [],
                     makeFreeId(0, defaultEqAndPrintCode(), false, basisDescription "PolyML.ptProperties"), declInBasis);
             val PtProperties = mkTypeConstruction ("ptProperties", propConstr, [], declInBasis)
 

mlsource/MLCompiler/ParseTree/TYPECHECK_PARSETREE.sml
@@ -997,17 +997,17 @@ struct
                         let
                             val description = { location = nameLoc, name = name, description = "" }
                         in
-                            makeTypeConstructor (name,
+                            makeTypeConstructor (name, typeVars,
                                 makeTypeId(isEqtype, false, (typeVars, EmptyType), description), props)
                         end
                         else case typeNameRebinding(typeVars, decType) of
                             SOME typeId =>
-                                makeTypeConstructor (name, typeId, props)
+                                makeTypeConstructor (name,  typeVars,typeId, props)
                         |   NONE =>
                             let
                                 val description = { location = nameLoc, name = name, description = "" }
                             in
-                                makeTypeConstructor (name,
+                                makeTypeConstructor (name, typeVars,
                                     makeTypeId(isEqtype, false, (typeVars, decType), description), props)
                             end
                 in
@@ -1050,7 +1050,7 @@ struct
                     val locations = [DeclaredAt newLoc, SequenceNo (newBindingId lex)]
                     (* Create a new constructor with the same unique ID. *)
                     val typeID = tcIdentifier tcons
-                    val newTypeCons = makeTypeConstructor(newName, typeID, locations)
+                    val newTypeCons = makeTypeConstructor(newName, tcTypeVars tcons, typeID, locations)
     
                     (* Copy the value constructors. *)
                     fun copyAConstructor(Value{name=cName, typeOf, class, access, ...}) =
@@ -1927,7 +1927,7 @@ struct
                     then makeTypeId(false, true, (typeVars, EmptyType), description)
                     else makeFreeIdEqUpdate (arity, Local{addr = ref ~1, level = ref baseLevel}, false, description)
                 val locations = [DeclaredAt nameLoc, SequenceNo (newBindingId lex)]
-                val tc = makeTypeConstructor(name, newId, locations)
+                val tc = makeTypeConstructor(name, typeVars, newId, locations)
             in
                 tcon := TypeConstrSet(tc, []);
                 enterType(TypeConstrSet(tc, []), name);
@@ -1963,7 +1963,7 @@ struct
                    right-hand side of the declaration. *)
                 val locations = [DeclaredAt nameLoc, SequenceNo (newBindingId lex)]
                 val tcon =
-                    makeTypeConstructor (name,
+                    makeTypeConstructor (name, typeVars,
                         makeTypeId(false, false, (typeVars, decType), description), locations)
                 val tset = TypeConstrSet(tcon, [])
             in

mlsource/MLCompiler/SIGNATURES.sml
@@ -1157,7 +1157,7 @@ struct
                                 |   constrs =>
                                     let
                                         val newTy =
-                                            makeTypeConstructor(tcName ty, tcIdentifier ty, tcLocations ty)
+                                            makeTypeConstructor(tcName ty, tcTypeVars ty, tcIdentifier ty, tcLocations ty)
                                     in
                                         TypeConstrSet(newTy, List.map copyConstructor constrs)
                                     end;

mlsource/MLCompiler/STRUCTURES_.ML
@@ -972,9 +972,7 @@ struct
     let
         val candidName : string = tcName candid;
         val targetName : string = tcName target;
-        val tvars =
-            List.tabulate(tcArity target  (* either will do *),
-                fn _ => mkTypeVar(generalisable, false, false, false))
+        val tvars = List.map TypeVar (tcTypeVars target); (* either will do *)
         (* If we get an error in the datatype itself print the full datatype. *)
         val printTypeEnv = { lookupType = fn _ => NONE, lookupStruct = fn _ => NONE }
         val errorInDatatype =
@@ -1625,7 +1623,7 @@ struct
                         val (newConstrs, newAddrMatch) = List.foldl convertConstructor ([], (addrs, matches)) valConstrs
                         val newConstructor =
                             makeTypeConstructor(
-                                tcName typConstr, tcIdentifier typConstr, tcLocations typConstr)
+                                tcName typConstr, tcTypeVars typConstr, tcIdentifier typConstr, tcLocations typConstr)
                     in
                         #enterType finalEnv (dName, TypeConstrSet(newConstructor, List.rev newConstrs));
                         newAddrMatch

mlsource/MLCompiler/STRUCTVALSIG.sml
@@ -88,6 +88,7 @@ sig
         TypeConstrs of
         {
             name:       string,
+            typeVars:   typeVarForm list,
             identifier: typeId,
             locations:  locationProp list (* Location of declaration *)
         }
@@ -208,6 +209,7 @@ sig
 
     val tcName:            typeConstrs -> string
     val tcArity:           typeConstrs -> int
+    val tcTypeVars:        typeConstrs -> typeVarForm list
     val tcEquality:        typeConstrs -> bool
     val tcSetEquality:     typeConstrs * bool -> unit
     val tcIdentifier:      typeConstrs -> typeId
@@ -215,7 +217,7 @@ sig
     val tcIsAbbreviation:  typeConstrs -> bool
 
     val makeTypeConstructor:
-        string * typeId * locationProp list -> typeConstrs
+        string * typeVarForm list * typeId * locationProp list -> typeConstrs
 
     datatype typeConstrSet = (* A type constructor with its, possible, value constructors. *)
         TypeConstrSet of typeConstrs * values list

mlsource/MLCompiler/STRUCT_VALS.ML
@@ -150,6 +150,7 @@ struct
         TypeConstrs of
         {
             name:       string,
+            typeVars:   typeVarForm list,
             identifier: typeId,
             locations:  locationProp list (* Location of declaration *)
         }
@@ -459,6 +460,7 @@ struct
      flag indicating if the values can be tested for equality. *)
       
     fun tcName       (TypeConstrs {name,...})       = name
+    fun tcTypeVars   (TypeConstrs {typeVars,...})   = typeVars
     fun tcIdentifier (TypeConstrs {identifier,...}) = identifier
     fun tcLocations  (TypeConstrs {locations, ...}) = locations
 
@@ -476,11 +478,13 @@ struct
     val tcEquality = isEquality o tcIdentifier;
     fun tcSetEquality(tc, eq) = setEquality(tcIdentifier tc, eq)
 
-    (* Construct a type constructor. *)
-    fun makeTypeConstructor (name, uid, locations) =
+    (* Construct a type constructor.  We need typeVars here purely to be
+       able to print datatypes with equality type variables e.g. datatype ''a t. *)
+    fun makeTypeConstructor (name, typeVars, uid, locations) =
         TypeConstrs
         {
             name       = name,
+            typeVars   = typeVars,
             identifier = uid,
             locations = locations
         }

mlsource/MLCompiler/TYPE_TREE.ML
@@ -275,7 +275,7 @@ struct
         end
 
         fun makeConstr(name, fullName, eqFun, boxed) =
-            makeTypeConstructor (name,
+            makeTypeConstructor (name, [],
                 makeFreeId(0, eqAndPrintCode(eqFun, 0, boxed), true, basisDescription fullName),
                 [DeclaredAt inBasis])
     
@@ -382,27 +382,30 @@ struct
         val refConstr =
             makeTypeConstructor 
                 ("ref",
+                [makeTv {value=EmptyType, level=generalisable, equality=false, nonunifiable=false, printable=false}],
                 makeFreeId(1, eqAndPrintCode(wordEq, 1, boxedAlways), true, basisDescription "ref"),
                 [DeclaredAt inBasis]);
         val arrayConstr =
             makeTypeConstructor 
                 ("array",
+                [makeTv {value=EmptyType, level=generalisable, equality=false, nonunifiable=false, printable=false}],
                 makeFreeId(1, eqAndPrintCode(wordEq, 1, boxedAlways), true, basisDescription "Array.array"),
                 [DeclaredAt inBasis]);
         val array2Constr =
             makeTypeConstructor 
                 ("array",
+                [makeTv {value=EmptyType, level=generalisable, equality=false, nonunifiable=false, printable=false}],
                 makeFreeId(1, eqAndPrintCode(wordEq, 1, boxedAlways), true, basisDescription "Array2.array"),
                 [DeclaredAt inBasis]);
         val byteArrayConstr =
             makeTypeConstructor 
-                ("byteArray",
+                ("byteArray", [],
                 makeFreeId(0, eqAndPrintCode(wordEq, 0, boxedAlways), true, basisDescription "byteArray"),
                 [DeclaredAt inBasis]);
         (* Bool is a datatype.  The constructors are added in INITIALISE. *)
         val boolConstr =
             makeTypeConstructor 
-                ("bool", makeFreeId(0, eqAndPrintCode(wordEq, 0, boxedNever), true, basisDescription "bool"),
+                ("bool", [], makeFreeId(0, eqAndPrintCode(wordEq, 0, boxedNever), true, basisDescription "bool"),
                 [DeclaredAt inBasis]);
     end
 
@@ -429,7 +432,7 @@ struct
                 }
         in
             makeTypeConstructor (
-                name, makeFreeId(0, Global (genCode(code, [], 0) ()), false, descr),
+                name, [], makeFreeId(0, Global (genCode(code, [], 0) ()), false, descr),
                 [DeclaredAt inBasis])
         end
     in
@@ -443,7 +446,7 @@ struct
 
     (* The unit type is equivalent to the empty record. *)
     val unitConstr   =
-        makeTypeConstructor ("unit",
+        makeTypeConstructor ("unit", [],
             makeTypeFunction({ location = inBasis, description = "unit", name = "unit" },
             ([], LabelledType {recList = [], fullList = FieldList([], true)})),
             [DeclaredAt inBasis]);
@@ -778,7 +781,7 @@ struct
                 if identical (equiv, copiedEquiv)
                 then tcon (* Type is identical and we don't want to change the name. *)
                 else (* How do we find a type function? *)
-                    makeTypeConstructor (mungeName(tcName tcon),
+                    makeTypeConstructor (mungeName(tcName tcon), args,
                         TypeId {
                             access = access, description = description, idKind = TypeFn(args, copiedEquiv)},
                             tcLocations tcon)
@@ -819,7 +822,7 @@ struct
                                 val newName = mungeName(tcName tcon)
                             in
                                 (*print(concat[tcName tcon, " not cached\n"]);*)
-                                makeTypeConstructor(newName, newId, tcLocations tcon)
+                                makeTypeConstructor(newName, tcTypeVars tcon, newId, tcLocations tcon)
                             end
                     end
             )
@@ -1197,13 +1200,6 @@ struct
                 end
            |    SOME (Names {name,...}) => name
     end (* varNameSequence *)
-  
-
-    (* Create the type variables on the fly. *)
-    fun tcTypeVars tc =
-        List.tabulate(tcArity tc,
-            fn _ => makeTv{value=EmptyType, level=generalisable, nonunifiable=false,
-                         equality=false, printable=false})
 
     (* Print a type (as a block of items) *)
     fun displayWithMap (t : types, depth : FixedInt.int, env, sigMap) =
@@ -2638,7 +2634,9 @@ struct
         let
             val typeV = varNameSequence ()
             (* Construct a ('a, 'b, 'c) tyCons construction for the result types
-               of each of the constructors. *)
+               of each of the constructors.  N.B. We use the original type
+               constructors because they have the appropriate equality type properties.
+               datatype 'a t = A of 'a is not the same as ''a t = A of ''a. *)
             val typeVars = tcTypeVars tCons
             val typeResult = mkTypeConstruction(name, tCons, map TypeVar typeVars, locations)
 
@@ -2940,7 +2938,7 @@ struct
                 val declDescription =
                     { location = declLoc, name = name, description = "Constructed from a free type variable." }
                 val tCons =
-                    makeTypeConstructor (name,
+                    makeTypeConstructor (name, [],
                         makeFreeId(0, Global(printAndEqCode()), tvEquality tvar, declDescription),
                         [DeclaredAt declLoc]);
                 val newVal = mkTypeConstruction(name, tCons, [], [])